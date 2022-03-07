79384242515d83e2fadf73962d8d443dbfad499f
Qifan Deng <plocircle@live.com>
2022-03-07 15:48:22 +0800

    [Game] playable

--- header/game.h
+++ header/game.h
@@ -1,23 +1,27 @@
 //
 // Created by Qifan Deng on 2022/3/7.
 //
-#include "stdio.h"
-
 #ifndef FIVEINAROW_GAME_H
+
 #define FIVEINAROW_GAME_H
 
+#include "stdio.h"
+#include "stdlib.h"
+
+#define BUFF_SIZE 256
 #define HEIGHT 15
 #define WIDTH 15
-
+#define END_VALUE 5
 #define NOT_OCCUPIED 0
 #define WHITE -1
 #define BLACK 1
 
-#define TRUE 0
-#define FALSE 1
+#define TRUE 1
+#define FALSE 0
 #define STONE_B '@'
 #define STONE_W '#'
 #define BOARD '+'
+
 int gBoard[HEIGHT][WIDTH];
 int gLastRound;
 
@@ -29,6 +33,8 @@ void showBoard();
 
 int placeAndShow(int stone, int row, int col);
 
-void playerPlace();
+void playerPlace(int *placedRow, int *placedCol);
+
+int isGameEnd(int row, int col);
 
 #endif //FIVEINAROW_GAME_H
--- src/game.c
+++ src/game.c
@@ -14,8 +14,17 @@ void startGame() {
     initBoard();
     printf("[*] Game started.\r\n");
     showBoard();
+    int row = 0;
+    int col = 0;
     while (1) {
-        playerPlace();
+        playerPlace(&row, &col);
+        if (isGameEnd(row, col) == TRUE)
+            break;
+    }
+    if (gLastRound == WHITE) {
+        printf("[*] White stone won!\r\n");
+    } else {
+        printf("[*] Black stone won!\r\n");
     }
 }
 
@@ -25,7 +34,6 @@ int place(int stone, int row, int col) {
     if (gBoard[row][col] != 0) return FALSE;
     if (stone != WHITE && stone != BLACK) return FALSE;
     gBoard[row][col] = stone;
-    printf("[*] Placed %d at (%d, %d)\r\n", stone, row, col);
     if (gLastRound == WHITE) gLastRound = BLACK;
     else gLastRound = WHITE;
     return TRUE;
@@ -73,34 +81,161 @@ int placeAndShow(int stone, int row, int col) {
     return ret;
 }
 
-void playerPlace() {
-    char position[3];
+char position[BUFF_SIZE];
+
+void playerPlace(int *placedRow, int *placedCol) {
+
     if (gLastRound == WHITE) {
         printf("[*] Black stone round, please place(%c): ", STONE_B);
     } else {
         printf("[*] White stone round, please place(%c): ", STONE_W);
     }
     scanf("%s", position);
+    printf("\r\n");
 
     int col = position[0] - 'a';
-    if (col < 0 || col >= WIDTH) {
+    if (col < 0 || col > WIDTH) {
         printf("[!] Incorrect position.\r\n");
-        return playerPlace();
+        return playerPlace(placedRow, placedCol);
     }
     int row = 0;
     if (position[2] - '0' >= 0) {
         row += position[2] - '0';
-        row += 10 * (position[2] - '0');
+        row += 10 * (position[1] - '0');
     } else {
         row += position[1] - '0';
     }
-    if (row < 0 || row >= HEIGHT) {
+    if (row < 0 || row > HEIGHT) {
         printf("[!] Incorrect position.\r\n");
-        return playerPlace();
+        return playerPlace(placedRow, placedCol);
     }
+    printf("[*] Placed at (%c, %d)\r\n", 'a' + col, row);
     row = HEIGHT - row;
     if (gLastRound == WHITE) {
         placeAndShow(BLACK, row, col);
     }
     placeAndShow(WHITE, row, col);
+    *placedRow = row;
+    *placedCol = col;
+
+}
+
+int abs(int value) {
+    if (value < 0) {
+        return -value;
+    }
+    return value;
+}
+
+int checkLeft(int row, int col) {
+    int value = 0;
+    if (col < END_VALUE - 1) return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row][col - i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkRight(int row, int col) {
+    int value = 0;
+    if (col >= WIDTH - END_VALUE) return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row][col + i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkUp(int row, int col) {
+    int value = 0;
+    if (row < END_VALUE - 1) return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row - i][col];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkDown(int row, int col) {
+    int value = 0;
+    if (row > HEIGHT - END_VALUE) return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row + i][col];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+
+int checkUpRight(int row, int col) {
+    int value = 0;
+    if (row < END_VALUE - 1 || col >= WIDTH - END_VALUE)
+        return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row - i][col + i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkUpLeft(int row, int col) {
+    int value = 0;
+    if (row < END_VALUE - 1 || col < END_VALUE - 1) return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row - i][col - i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkDownLeft(int row, int col) {
+    int value = 0;
+    if (row >= HEIGHT - END_VALUE || col < END_VALUE - 1)
+        return
+                FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row + i][col - i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int checkDownRight(int row, int col) {
+    int value = 0;
+    if (row >= HEIGHT - END_VALUE || col >= WIDTH - END_VALUE)
+        return FALSE;
+    for (int i = 0; i < END_VALUE; ++i) {
+        value += gBoard[row + i][col + i];
+    }
+    if (abs(value) == END_VALUE) return TRUE;
+    return FALSE;
+}
+
+int isGameEnd(int row, int col) {
+    if (checkUp(row, col)) {
+        return TRUE;
+    }
+    if (checkDown(row, col)) {
+        return TRUE;
+    }
+    if (checkLeft(row, col)) {
+        return TRUE;
+    }
+    if (checkRight(row, col)) {
+        return TRUE;
+    }
+    if (checkUpRight(row, col)) {
+        return TRUE;
+    }
+    if (checkUpLeft(row, col)) {
+        return TRUE;
+    }
+    if (checkDownLeft(row, col)) {
+        return TRUE;
+    }
+    if (checkDownRight(row, col)) {
+        return TRUE;
+    }
+    return FALSE;
 }
